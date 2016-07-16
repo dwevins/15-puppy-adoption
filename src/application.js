7181020a7888bf2691476d737f9f7b9fce8f66e6
Danny Evins <dcastkid@gmail.com>
2016-07-16 17:42:51 -0500

    more js, copy html to puppyView so it can be generated

--- src/application.js
+++ src/application.js
@@ -1,16 +1,23 @@
 'use strict';
 
 import PuppyView from 'puppy-view';
+import CreateFormView from 'create-form-view';
 
 export default class ApplicationView {
   constructor(parent) {
     this.data = [];
     this.parent = parent;
     this.fetchURL = 'http://tiny-tn.herokuapp.com/collections/dwe-puppies';
+    this.List = parent.querySelector('.app-list');
+    this.view = '';
+  }
+
+  start() {
+    this.view = new CreateFormView(this.parent, this);
+    this.render();
   }
 
   render() {
-    this.getFetch();
     this.data.map((obj) => {
       return new PuppyView(obj, this.parent);
     });
--- src/puppy-view.js
+++ src/puppy-view.js
@@ -1,1 +1,50 @@
 'use strict';
+
+export default class PuppyView {
+  constructor(puppy, app) {
+    this.puppy = puppy;
+    this.app = app;
+    this.puppyCard = document.createElement('li');
+    this.populateCard();
+  }
+
+  populateCard() {
+    this.generateCardSkeleton();
+    this.fillCardInfo();
+  }
+
+  generateCardSkeleton() {
+    this.puppyCard.innerHTML = `
+    <div class="item-image">
+      <div class="item-image__container">
+        <img src="http://placecage.com/200/200" alt="" class="item-image__container--img">
+      </div>
+    </div>
+    <div class="item-details form">
+        <div class="item-details__field form__field">
+          <p class="item-details__field--title form__field--title">Name</p>
+          <input type="text" class="item-details__field--input form__field--input">
+        </div>
+        <div class="item-details__field">
+          <p class="item-details__field--title form__field--title">Age</p>
+          <input type="text" class="item-details__field--input form__field--input">
+        </div>
+        <div class="item-details__field">
+          <p class="item-details__field--title form__field--title">Photo URL</p>
+          <input type="text" class="item-details__field--input form__field--input">
+        </div>
+        <div class="item-details__field">
+          <p class="item-details__field--title form__field--title">Profile</p>
+          <input type="text" class="item-details__field--input form__field--input">
+        </div>
+      <div class="item-details__buttons form__buttons">
+        <button class="details-btn form__buttons--btn">Adopt</button>
+        <button class="details-btn form__buttons--btn">Update</button>
+      </div>
+    </div>`;
+  }
+
+  fillCardInfo() {
+
+  }
+}
